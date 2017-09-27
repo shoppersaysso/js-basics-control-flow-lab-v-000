b9face3e20e63e598f208e30e1fffa2ff1ece76c
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 02:40:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(num) {
+
+  if (num <= 400) {
+    return 'This one is on me!';
+  } else {
+    if (num >= 2000 && num < 2500) {
+      return 'I will gladly take your thirty bucks.';
+    } else {
+      if (num >= 2500) {
+      return 'No can do.';
+      }
+    }
+  }
+}
+
+function ternaryCheckCity(city) {
+
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip) {
+
+  switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+  }
+}
