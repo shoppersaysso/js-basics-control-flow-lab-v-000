1bd78ce911cb75640e55704fd2ff28cb6d2b7a08
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 02:33:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(num) {
   if (num <= 400) {
     return 'This one is on me!';
   } else {
-    if (num <= 2000 && num >= 401) {
+    if (num >= 2000) {
       return 'I will gladly take your thirty bucks.';
     } else {
       return 'No can do.';
