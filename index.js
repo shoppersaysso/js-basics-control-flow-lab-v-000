52dde8d33c2634ea9f0508391ca37965862d02a4
Sara Hopper <sara.hopper@gmail.com>
2017-09-27 02:39:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,4 +24,13 @@ function ternaryCheckCity(city) {
 
 function switchOnCharmFromTip(tip) {
 
+  if (tip === 'generous') {
+    return 'Thank you so much.'
+  } else {
+    if (tip === 'not so generous') {
+      return 'Thank you.'
+    } else {
+      return 'Bye.'
+    }
+  }
 }
